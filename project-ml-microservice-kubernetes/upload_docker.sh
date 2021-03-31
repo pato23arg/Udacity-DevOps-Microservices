69e4343be38bd477ffebec0e9e9e5c60929be221
EC2 Default User <ec2-user@ip-172-31-93-84.ec2.internal>
2021-03-31 16:14:10 +0000

    -- docker section finished --

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=pato23arg/api-endpoint
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login && docker image tag api-endpoint $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
